60e14f3108915d315f525116683c8b1c83085c8e
SoraKumo <info@croud.jp>
2021-11-30 17:02:06 +0900

    Add time display

---
+++ README.md
@@ -0,0 +1,11 @@
+# Sample of @node-libraries/wasm-webp-encoder
+
+# Overview
+
+Here is a sample library for encoding webp.
+
+<https://www.npmjs.com/package/@node-libraries/wasm-webp-encoder>
+
+# Operation check
+
+<https://next-webp.vercel.app/>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { encode } from "@node-libraries/wasm-webp-encoder";
 import styled from "./index.module.scss";
 
@@ -8,43 +8,52 @@ export const classNames = (...classNames: (string | undefined | false)[]) =>
     ""
   ) as string | undefined;
 
-export const convertWebp = async (blob: Blob) => {
-  if (!blob.type.match(/^image\/(png|jpeg)/)) return blob;
-  const src = await blob
-    .arrayBuffer()
-    .then(
-      (v) => `data:${blob.type};base64,` + Buffer.from(v).toString("base64")
-    );
-  const img = document.createElement("img");
-  img.src = src;
-  await new Promise((resolve) => (img.onload = resolve));
-  const canvas = document.createElement("canvas");
-  [canvas.width, canvas.height] = [img.width, img.height];
-  const ctx = canvas.getContext("2d")!;
-  ctx.drawImage(img, 0, 0);
-  const value = await encode(ctx.getImageData(0, 0, img.width, img.height));
-  if (!value) return null;
-  return new Blob([value], { type: "image/webp" });
+const Time = () => {
+  const [time, setTime] = useState(0);
+  useEffect(() => {
+    const handle = setInterval(() => setTime((v) => v + 1), 100);
+    return () => clearInterval(handle);
+  }, []);
+  return <div>{time}</div>;
 };
 
 const Page = () => {
   const ref = useRef<HTMLInputElement>(null);
   const [isDrag, setDrag] = useState(false);
   const [imageData, setImageData] = useState<string | undefined>();
-  const convertUrl = async (blob: Blob | undefined | null) => {
-    if (!blob) return undefined;
-    return (
-      `data:image/webp;base64,` +
-      Buffer.from(await blob.arrayBuffer()).toString("base64")
-    );
+  const [state, setState] = useState<"idle" | "progress" | "finished">("idle");
+  const convertUrl = (data: ArrayBuffer | undefined | null) => {
+    if (!data) return undefined;
+    return `data:image/webp;base64,` + Buffer.from(data).toString("base64");
   };
+
+  const convertWebp = async (blob: Blob) => {
+    if (!blob.type.match(/^image\/(png|jpeg|gif)/)) return null;
+    setState("progress");
+    const src = await blob
+      .arrayBuffer()
+      .then(
+        (v) => `data:${blob.type};base64,` + Buffer.from(v).toString("base64")
+      );
+    const img = document.createElement("img");
+    img.src = src;
+    await new Promise((resolve) => (img.onload = resolve));
+    const canvas = document.createElement("canvas");
+    [canvas.width, canvas.height] = [img.width, img.height];
+    const ctx = canvas.getContext("2d")!;
+    ctx.drawImage(img, 0, 0);
+    const value = await encode(ctx.getImageData(0, 0, img.width, img.height));
+    setState("finished");
+    return value;
+  };
+
   useEffect(() => {
     const handle = () => {
       navigator.clipboard.read().then((items) => {
         for (const item of items) {
           item.getType("image/png").then(async (value) => {
             const v = await convertWebp(value);
-            convertUrl(v).then(setImageData);
+            setImageData(convertUrl(v));
           });
         }
       });
@@ -53,63 +62,69 @@ const Page = () => {
     return () => removeEventListener("paste", handle);
   }, []);
   return (
-    <div
-      className={classNames(styled.root, isDrag && styled.dragover)}
-      onDragOver={(e) => {
-        e.preventDefault();
-        e.stopPropagation();
-      }}
-      onClick={(e) => {
-        ref.current?.click();
-        e.stopPropagation();
-      }}
-      onDragEnter={() => setDrag(true)}
-      onDragLeave={() => setDrag(false)}
-      onDrop={(e) => {
-        for (const item of e.dataTransfer.files) {
-          convertWebp(item).then((blob) => {
-            convertUrl(blob).then(setImageData);
-          });
-        }
-        e.preventDefault();
-      }}
-    >
-      {imageData ? (
-        <>
-          <span
-            className={styled.clear}
-            onClick={() => {
-              setImageData(undefined);
-            }}
-          >
-            ✖
-          </span>
-          <img
-            src={imageData}
-            onClick={() => {
-              const node = document.createElement("a");
-              node.download = "download.webp";
-              node.href = imageData;
-              node.click();
-            }}
-          />
-        </>
-      ) : (
-        <>
-          <input
-            ref={ref}
-            type="file"
-            accept=".jpg, .png, .gif"
-            onChange={(e) => {
-              const blob = e.currentTarget.files?.[0];
-              if (blob) {
-                convertUrl(blob).then(setImageData);
-              }
-            }}
-          />
-        </>
-      )}
-    </div>
+    <>
+      <div>
+        <a href="https://github.com/SoraKumo001/next-webp">Source code</a>
+      </div>
+      <Time />
+      <div
+        className={classNames(styled.root, isDrag && styled.dragover)}
+        onDragOver={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+        }}
+        onClick={(e) => {
+          ref.current?.click();
+          e.stopPropagation();
+        }}
+        onDragEnter={() => setDrag(true)}
+        onDragLeave={() => setDrag(false)}
+        onDrop={(e) => {
+          for (const item of e.dataTransfer.files) {
+            convertWebp(item).then((v) => setImageData(convertUrl(v)));
+          }
+          e.preventDefault();
+        }}
+      >
+        {imageData ? (
+          <>
+            <span
+              className={styled.clear}
+              onClick={() => {
+                setImageData(undefined);
+                setState("idle");
+              }}
+            >
+              ✖
+            </span>
+            <img
+              src={imageData}
+              onClick={() => {
+                const node = document.createElement("a");
+                node.download = "download.webp";
+                node.href = imageData;
+                node.click();
+              }}
+            />
+          </>
+        ) : (
+          <>
+            {state === "progress" && <div>Conversion in progress.</div>}
+            <input
+              ref={ref}
+              type="file"
+              accept=".jpg, .png, .gif"
+              onChange={(e) => {
+                const blob = e.currentTarget.files?.[0];
+                if (blob) {
+                  convertWebp(blob).then((v) => setImageData(convertUrl(v)));
+                }
+              }}
+            />
+          </>
+        )}
+      </div>
+    </>
   );
 };
 export default Page;
